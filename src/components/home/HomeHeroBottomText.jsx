edbc15e5e37eb5da6be15c43157e90aa7d6cccc9
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-12 13:24:54 +0530

    Home Hero Bottom

--- src/components/home/HomeHeroBottomText.jsx
+++ src/components/home/HomeHeroBottomText.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeHeroBottomText = () => {
   return (
-    <div>HomeHeroBottomText</div>
+    <div className='text-white font-[font300] flex items-center justify-center gap-5 mb-2'>
+      <Link className='text-[6vw] border-2 border-white/90 rounded-full px-5 pt-2 pb-0 uppercase leading-[5.5vw]'>Projects</Link>
+      <Link className='text-[6vw] border-2 border-white/90 rounded-full px-5 pt-2 pb-0 uppercase leading-[5.5vw]'>Agence</Link>
+    </div>
   )
 }
 
--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -3,9 +3,9 @@ import Video from './Video'
 
 const HomeHeroText = () => {
   return (
-    <div className='font-[font500] text-white pt-5 text-center'>
+    <div className='font-[font300] text-white pt-5 text-center'>
       <div className='text-[8.5vw] uppercase leading-[8vw]'>L'étincelle</div>
-      <div className='text-[8.5vw] flex -mt-1 uppercase leading-[8vw] justify-center items-center'>qui
+      <div className='text-[8.5vw] flex -mt-2 px-6 uppercase leading-[8vw] justify-center items-center'>qui
           <div className='h-[8vw] w-[15vw] rounded-full overflow-hidden'><Video /></div>
         génère</div>
       <div className='text-[8.5vw] uppercase leading-[8vw]'>la créativité</div>
