8919b3b0a72324dc03b7b20dcb3cbe9093b5674d
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-27 17:03:28 +0530

    agence section2 marquee setup

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -61,8 +61,23 @@ const Agence = () => {
       </div>
     </div>
    </div>
-   <div className='section2 h-screen'>
-     
+   <div className='section2 h-[90vh] font-[font500] w-[100%] px-25 text-[1.5vw] bg-red-50 flex justify-around items-center gap-20'>
+     <div className=''>
+      <h2>Nos projets_ naissent dans l’humilité, grandissent dans la curiosité et vivent grâce à la créativité sous toutes ses formes.</h2>
+     </div>
+     <div>Notre création_ bouillonne dans un environnement où le talent a le goût d’exploser. Où on se sent libre d’être la meilleure version de soi-même.</div>
+     <div>Notre culture_ c’est l’ouverture aux autres. Point. Tout l’équipage participe à bâtir une agence dont on est fiers.</div>
+   </div>
+
+   <div className='marqueePart relative bg-black h-[170vh] rounded-t-4xl '>
+    <div className='marqueeReverse w-full bg-red-200 absolute top-[490px] left-5 uppercase text-lime-200 text-[22vh] font-[font500] leading-[18vh] pt-4 z-1 whitespace-nowrap'>Olivier</div>
+    <div className='pt-60 flex items-center justify-center'>
+      <img className='rounded-3xl z-2' src="https://k72.ca/images/teamMembers/Olivier_640X960.jpg?w=640&h=960&s=9b715851e76624b3d00c6715af5c47cd" alt="" />
+    </div>
+    <div className='marqueeForward z-3 absolute top-[820px] right-20 uppercase text-lime-300 text-[22vh] font-[font500] leading-[18vh] pt-4 whitespace-nowrap'>
+      duclos
+    </div>
+      <div className='smallText text-white font-[font500] text-[3vh] absolute right-10 bottom-70 hover:text-lime-300 duration-200 transition-colors cursor-cell'>Designer Graphiques</div>
    </div>
    </>
   )
