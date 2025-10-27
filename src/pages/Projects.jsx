c3fd2a4183337b4dd41dda1fcdca400565b36303
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-27 09:59:59 +0530

    Projects Section initial

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProjectsCard from '../components/projects/ProjectsCard'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+    <div className='p-2'>
+     <div className='pt-[40vh]'>
+       <h2 className='font-[font500] text-[10vw] uppercase'>Projets</h2>
+     </div>
+      <div className='outerDiv -mt-15'>
+        <ProjectsCard />
+      </div>
+    </div>
+    </>
   )
 }
 
