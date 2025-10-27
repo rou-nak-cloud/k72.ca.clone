ea2162accb354b62ba2f829cc40df601e557348a
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-27 14:58:47 +0530

    projects cards adding

--- src/components/projects/ProjectsCard.jsx
+++ src/components/projects/ProjectsCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const ProjectsCard = () => {
+const ProjectsCard = (props) => {
   return (
     <>
-      <div className='innerDivFull w-full h-[400px] flex gap-1 '>
+      <div className='innerDivFull w-full h-[600px] flex gap-1 mb-2'>
           <div className='brotherDiv group relative w-1/2 h-full hover:rounded-[30px] transition-all overflow-hidden'>
-            <img className='w-full h-full object-cover' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg" alt="" />
-            <div className='opacity-0 group-hover:opacity-100 transition-all duration-200 bg-black/20 absolute top-0 left-0 h-full w-full flex justify-center items-center cursor-cell'>
+            <img className='w-full h-full object-cover' src={props.image1} alt="" />
+            <div className='opacity-0 group-hover:opacity-100 transition-all duration-200 bg-black/30 absolute top-0 left-0 h-full w-full flex justify-center items-center cursor-cell'>
               <h2 className='uppercase text-4xl pt-1 px-2 font-[font300] border-3 border-white text-white rounded-full'>Voir de louto</h2>
             </div>
           </div>
-          <div className='brotherDiv group relative w-1/2 h-full hover:rounded-[30px] transition-all overflow-hidden'>
-            <img className='w-full h-full object-cover' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg" alt="" />
-            <div className='opacity-0 group-hover:opacity-100 transition-all duration-200 bg-black/20 absolute top-0 left-0 h-full w-full flex justify-center items-center cursor-cell'>
+          <div className='brotherDiv group relative w-1/2 h-full hover:rounded-[30px] transition-all overflow-hidden '>
+            <img className='w-full h-full object-cover' src={props.image2} alt="" />
+            <div className='opacity-0 group-hover:opacity-100 transition-all duration-200 bg-black/30 absolute top-0 left-0 h-full w-full flex justify-center items-center cursor-cell'>
               <h2 className='uppercase text-4xl pt-1 px-2 font-[font300] border-3 border-white text-white rounded-full'>Voir de louto</h2>
             </div>
           </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ProjectsCard from '../components/projects/ProjectsCard'
 
 const Projects = () => {
+
+    const projects = [{
+    image1: 'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+  }]
+
   return (
     <>
     <div className='p-2'>
@@ -9,7 +21,9 @@ const Projects = () => {
        <h2 className='font-[font500] text-[10vw] uppercase'>Projets</h2>
      </div>
       <div className='outerDiv -mt-15'>
-        <ProjectsCard />
+        {projects.map((elem,idx)=>(
+           <ProjectsCard key={idx} image1={elem.image1} image2={elem.image2} />
+        ))}
       </div>
     </div>
     </>
