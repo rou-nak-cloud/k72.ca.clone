ab7c815805cf496f0e0426ccd0194407247a3259
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-31 16:56:07 +0530

    update index css for animation

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -74,7 +74,7 @@ const Agence = () => {
     <div className='pt-60 flex items-center justify-center'>
       <img className='rounded-3xl z-2' src="https://k72.ca/images/teamMembers/Olivier_640X960.jpg?w=640&h=960&s=9b715851e76624b3d00c6715af5c47cd" alt="" />
     </div>
-    <div className='marqueeForward moveX z-3 absolute top-[820px] right-20 uppercase text-lime-300 text-[22vh] font-[font500] leading-[18vh] pt-4 whitespace-nowrap'>
+    <div className='marqueeForward moveY z-3 absolute top-[820px] right-20 uppercase text-lime-300 text-[22vh] font-[font500] leading-[18vh] pt-4 whitespace-nowrap'>
       duclos
     </div>
       <div className='smallText text-white font-[font500] text-[3vh] absolute right-10 bottom-70 hover:text-lime-300 duration-200 transition-colors cursor-cell'>Designer Graphiques</div>
