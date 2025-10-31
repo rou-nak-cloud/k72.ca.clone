76adf1421976cb3c067dc9bf7af06705cb41a0b4
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-31 17:29:10 +0530

    nav logo color context

--- src/components/context/NavContext.jsx
+++ src/components/context/NavContext.jsx
@@ -1,13 +1,26 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 export const NavbarContext = createContext();
+export const NavbarColorContext = createContext();
 
 const NavContext = ({children}) => {
   const [navOpen, setNavOpen] = useState(false)
+  const [navColor, setNavColor] = useState('white')
 
+  const locate = useLocation().pathname
+  useEffect(function(){
+    if(locate == '/projects' || locate == '/agence'){
+      setNavColor('black')
+    } else {
+      setNavColor('white')
+    }
+  },[locate])
   return (
     <>
       <NavbarContext.Provider value={[navOpen, setNavOpen]}>
-        {children}
+        <NavbarColorContext.Provider value={[navColor, setNavColor]}>
+          {children}
+        </NavbarColorContext.Provider>
       </NavbarContext.Provider>  
     </>
   )
