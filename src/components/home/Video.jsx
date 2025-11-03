5c7dc469076ac50976274112dc9a0a613030cb52
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-03 09:06:29 +0530

    updating path

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+      <video className='h-full w-full object-cover' autoPlay loop playsInline muted
+      src="/video.mp4"
+      // src="../../../public/video.mp4"
+      ></video>
     </div>
   )
 }
